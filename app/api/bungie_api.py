05af7dcf9eace1829ef7c3f2a36ccaaf134d0bca
anwersaad0 <anwer.saad0@gmail.com>
2024-03-01 18:10:34 -0800

    added auth url to api file

--- app/api/bungie_api.py
+++ app/api/bungie_api.py
@@ -3,3 +3,5 @@ import os
 API_KEY = os.environ.get("API_KEY")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+AUTH_URL = "https://www.bungie.net/en/OAuth/Authorize"
