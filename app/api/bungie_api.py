e65ef050e2112154692871b7b61a1191b57eb25d
anwersaad0 <anwer.saad0@gmail.com>
2024-03-04 19:39:11 -0800

    added other dependencies and variables to api file

--- app/api/bungie_api.py
+++ app/api/bungie_api.py
@@ -1,7 +1,14 @@
+from requests_oauthlib import OAuth2Session
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 API_KEY = os.environ.get("API_KEY")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
 AUTH_URL = "https://www.bungie.net/en/OAuth/Authorize"
+TOKEN_URL = "https://www.bungie.net/platform/app/oauth/token/"
+
+#session
