07684eece5c6a7c9b56c105dcf4f85fcf2b52096
anwersaad0 <anwer.saad0@gmail.com>
2024-04-03 15:09:42 -0700

    imported dispatch

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function HomePage() {
+    const dispatch = useDispatch();
+
+    //useEffect
+
 
     return (
         <main>
