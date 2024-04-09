7912ef092c94b882c4362d7c1606e6d76ea79045
anwersaad0 <anwer.saad0@gmail.com>
2024-04-09 12:19:09 -0700

    adding another test script to try more traditional flask routes

--- app/api/bungie_help.py
+++ app/api/bungie_help.py
@@ -1,3 +1,4 @@
+from flask import Blueprint, request
 import requests
 import os
 import pydest
@@ -7,3 +8,11 @@ CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
 destiny = pydest.Pydest(API_KEY)
+
+# deciding on establishing routes linking to API
+
+bungie_auth = Blueprint('bungie_auth', __name__)
+
+@bungie_auth.route('/')
+def index():
+    return False
