90eca50187955d3d8a765d66a0db27bb0c245efe
anwersaad0 <anwer.saad0@gmail.com>
2024-04-09 20:13:39 -0700

    added auth methods, frontend file for thunk tests

--- app/api/bungie_help.py
+++ app/api/bungie_help.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request
-import requests
+from flask import Blueprint, request, session
+from uuid import uuid4
+
 import os
 import pydest
 
@@ -7,12 +8,32 @@ API_KEY = os.environ.get("API_KEY")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
+AUTH_URL = "https://www.bungie.net/en/OAuth/Authorize"
+
 destiny = pydest.Pydest(API_KEY)
 
 # deciding on establishing routes linking to API
 
 bungie_auth = Blueprint('bungie_auth', __name__)
 
+def save_created_state(state):
+    session['state_token'] = state
+    pass
+
+def make_auth_url():
+    state = str(uuid4())
+    save_created_state(state)
+    return state
+
+def check_valid_state(state):
+    saved_state = session['state_token']
+    if state == saved_state:
+        print("It's a match!")
+        return True
+    else:
+        return False
+
 @bungie_auth.route('/')
 def index():
-    return False
+    #state = 
+    return 0
