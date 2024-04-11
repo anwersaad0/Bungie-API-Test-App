b52ed8db6116b750c11c7ae924d000cf9870414b
anwersaad0 <anwer.saad0@gmail.com>
2024-04-11 14:12:34 -0700

    setting up auth route in react

--- react-app/src/store/bungie_routes.js
+++ react-app/src/store/bungie_routes.js
@@ -1,10 +1,30 @@
+const CREATE_AUTH = "/CREATE_AUTH";
 
-const getItem = (item) => ({
-    payload: item,
-});
+const createAuth = (auth) => ({
+    type: CREATE_AUTH,
+    payload: auth,
+})
+
+// const getItem = (item) => ({
+//     payload: item,
+// });
+
+export const bungieAuthenticate = () => async (dispatch) => {
+    await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            'client_id': "{my-client-id}",
+            'grant_type': "authorization_code",
+            'code': authCode
+        }).toString()
+    })
+}
 
 const initState = {};
 
 export default function reducer(state = initState, action) {
-    
+
 }
