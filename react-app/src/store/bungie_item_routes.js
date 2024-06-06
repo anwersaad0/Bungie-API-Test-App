1ef104adc152e287a68b6f19d777b9c61edef100
anwersaad0 <anwer.saad0@gmail.com>
2024-06-05 17:35:39 -0700

    started work on character items route

--- react-app/src/store/bungie_item_routes.js
+++ react-app/src/store/bungie_item_routes.js
@@ -1,10 +1,16 @@
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const GET_D2_ITEM = "/GET_D2_ITEM";
+const GET_CHAR_ITEMS = "/GET_CHAR_ITEMS";
 
 const getItem = (item) => ({
     type: GET_D2_ITEM,
     item,
+})
+
+const getCharacterItems = (items) => ({
+    type: GET_CHAR_ITEMS,
+    items,
 })
 
 export const getD2Item = (itemHash) => async (dispatch) => {
@@ -22,8 +28,25 @@ export const getD2Item = (itemHash) => async (dispatch) => {
     }
 }
 
-export const getCharacterItems = (...hashes) => async (dispatch) => {
-    //...
+export const getD2CharacterItems = (...hashes) => async (dispatch) => {
+    const responses = [];
+
+    for (let hash of hashes) {
+        const hashRes = await fetch(`https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/${hash}`, {
+            method: "GET",
+            headers: {
+                "X-API-KEY": apiKey
+            }
+        });
+
+        if (hashRes.ok) {
+            const hashData = await hashRes.json();
+            responses.push(hashData)
+        }
+    }
+
+    dispatch(getCharacterItems(responses));
+    return responses;
 }
 
 const initState = {};
@@ -34,6 +57,12 @@ function bungieItemReducer(state = initState, action) {
         case GET_D2_ITEM:
             newState = {...state}
             newState[action.item] = action.item;
+            return newState;
+        case GET_CHAR_ITEMS:
+            newState = {...state}
+            action.items.forEach(item => {
+                newState[item.itemHash] = item;
+            })
             return newState;
         default:
             return state;
