c1a99241169eb4b77300f7296f7a63790ea6129e
anwersaad0 <anwer.saad0@gmail.com>
2024-06-05 17:52:43 -0700

    added store files and dotenv

---
+++ vite-project/src/store/bungie_manifest_routes.js
@@ -0,0 +1,60 @@
+const fs = require('browserify-fs');
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const GET_D2_MANIFEST = "/GET_D2_MANIFEST";
+const GET_DEFINITIONS = "/GET_DEFINITIONS";
+
+const getManifest = (manifest) => ({
+    type: GET_D2_MANIFEST,
+    manifest
+});
+
+const getDefinitions = (defs) => ({
+    type: GET_DEFINITIONS,
+    defs
+})
+
+export const getD2Manifest = () => async (dispatch) => {
+    const res = await fetch('https://www.bungie.net/Platform/Destiny2/Manifest');
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(getManifest(data));
+    }
+};
+
+export const getJsonDefinitions = () => async (dispatch) => {
+    const res = await fetch('https://www.bungie.net/common/destiny2_content/json/en/aggregate-04f359b1-4103-42a1-a9e1-dc398e1f079e.json');
+
+    if (res.ok) {
+        const data = await res.json();
+        //console.log('data', data);
+        //dispatch(getDefinitions(data));
+
+        // await fs.writeFile('./definitions.json', JSON.stringify(data.DestinyInventoryItemLiteDefinition), (err) => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        // })
+    }
+}
+
+const initState = {};
+
+function manifestReducer(state = initState, action) {
+    let newState;
+    switch(action.type) {
+        case GET_D2_MANIFEST:
+            newState = {...state}
+            newState[action.manifest] = action.manifest;
+            return newState;
+        case GET_DEFINITIONS:
+            newState = {...state}
+            newState[action.defs] = action.defs;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default manifestReducer;
---
+++ vite-project/src/store/bungie_routes.js
@@ -0,0 +1,41 @@
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const GET_D2_PROFILE = "/GET_D2_PROFILE";
+
+const getProfile = (profile) => ({
+    type: GET_D2_PROFILE,
+    profile,
+})
+
+export const getD2Profile = () => async (dispatch) => {
+    //console.log('api key there?', apiKey);
+
+    const res = await fetch(`https://www.bungie.net/Platform/Destiny2/3/Profile/4611686018483417802/?components=100,200,205`, {
+        method: "GET",
+        headers: {
+            "X-API-Key": apiKey
+        },
+    });
+
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(getProfile(data));
+        return data;
+    }
+}
+
+const initState = {};
+
+function bungieReducer(state = initState, action) {
+    let newState;
+    switch(action.type) {
+        case GET_D2_PROFILE:
+            newState = {...state}
+            newState[action.profile] = action.profile;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default bungieReducer;
