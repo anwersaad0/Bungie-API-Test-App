df6e47b34e4080ee1f31fe23e3daa0d8fb081347
anwersaad0 <anwer.saad0@gmail.com>
2024-06-10 14:15:28 -0700

    assembled reducer for player store, adding to D2PlayerSearch

--- vite-project/src/components/D2PlayerSearch/index.jsx
+++ vite-project/src/components/D2PlayerSearch/index.jsx
@@ -1,16 +1,58 @@
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { getPlayersByName, getPlayerByDisplayParams } from "../../store/bungie_player_routes";
 
-function D2PlayerSearch(...hashes) {
+function D2PlayerSearch() {
     const dispatch = useDispatch();
 
+    const players = useSelector((state) => state.players);
+    const playerDetailed = useSelector((state) => state.player);
+
+    const [username, setUsername] = useState('');
+    const [nameCode, setNameCode] = useState('');
+
     useEffect(() => {
-        
-    }, [dispatch]);
+        dispatch(getPlayersByName(username));
+        dispatch(getPlayerByDisplayParams(username, nameCode));
+    }, [username, nameCode, dispatch]);
+
+    const handleSearch = async (e) => {
+        e.preventdefault();
+
+        if (!username) return;
+
+
+    }
 
     return (
         <main>
+
+            <div>
+
+                <input 
+                    placeholder="Enter Player Name"
+                    onChange={e => setUsername(e.target.value)}
+                    className="player-search"
+                ></input>
+
+                <input
+                    type="number"
+                    onChange={e => setNameCode(e.target.value)}
+                    className="number-search"
+                ></input>
+
+            </div>
+
+            <div>
+
+                <div>
+
+
+
+                </div>
+
+            </div>
 
         </main>
     )
--- vite-project/src/store/bungie_player_routes.jsx
+++ vite-project/src/store/bungie_player_routes.jsx
@@ -1,14 +1,20 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const GET_PLAYERS = "/GET_PLAYERS";
+const GET_PLAYER_DETAILED = "/GET_PLAYER_DETAILED";
 
 const getPlayers = (players) => ({
     type: GET_PLAYERS,
     players,
 });
 
+const getPlayerDetailed = (player) => ({
+    type: GET_PLAYER_DETAILED,
+    player,
+})
+
 export const getPlayersByName = (playerName) => async (dispatch) => {
-    const res = await fetch("https://www.bungie.net/Platform/User/Search/GlobalName/:page/", {
+    const res = await fetch("https://www.bungie.net/Platform/User/Search/GlobalName/0/", {
         method: "POST",
         headers: {
             "X-API-Key": apiKey
@@ -25,7 +31,7 @@ export const getPlayersByName = (playerName) => async (dispatch) => {
     }
 }
 
-export const getPlayersByDisplayParams = (playerName, playerCode) => async (dispatch) => {
+export const getPlayerByDisplayParams = (playerName, playerCode) => async (dispatch) => {
     const res = await fetch("https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayerByBungieName/All/", {
         method: "POST",
         headers: {
@@ -39,7 +45,7 @@ export const getPlayersByDisplayParams = (playerName, playerCode) => async (disp
 
     if (res.ok) {
         const data = await res.json();
-        dispatch(getPlayers(data));
+        dispatch(getPlayerDetailed(data));
         return data;
     }
 }
@@ -51,7 +57,14 @@ function playersReducer(state = initState, action) {
     switch(action.type) {
         case GET_PLAYERS:
             newState = {...state};
-            newState
+            action.players.Response.searchResults.forEach(player => {
+                newState[player.bungieGlobalDisplayNameCode] = action.player
+            });
+            return newState;
+        case GET_PLAYER_DETAILED:
+            newState = {...state};
+            newState[action.player.Response] = action.player;
+            return newState;
         default:
             return state;
     }
