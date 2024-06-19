f7a1e41742f71301735686d4cf015eb54ae5b187
anwersaad0 <anwer.saad0@gmail.com>
2024-06-18 17:38:11 -0700

    minor changes to homepage

--- vite-project/src/components/HomePage/index.jsx
+++ vite-project/src/components/HomePage/index.jsx
@@ -3,9 +3,9 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 //import { getJsonDefinitions } from "../../store/bungie_manifest_routes";
 
-import { DefinitionsProvider, verbose, setApiKey, loadDefs, getInventoryItemDef, getInventoryItemLiteDef, getAllInventoryItemLiteDefs, includeTables,} from '@d2api/manifest-react';
+import { DefinitionsProvider, verbose, setApiKey, loadDefs, getInventoryItemDef, getInventoryItemLiteDef, getAllInventoryItemLiteDefs, includeTables } from '@d2api/manifest-react';
 
-import { getProfile, getCharacter, getVendor, BungieMembershipType, DestinyComponentType } from "bungie-api-ts/destiny2";
+import { getProfile, getCharacter, getVendor, BungieMembershipType, DestinyComponentType, SocketPlugSources } from "bungie-api-ts/destiny2";
 import { getBungieNetUserById, getMembershipDataForCurrentUser } from "bungie-api-ts/user";
 
 verbose();
@@ -21,30 +21,35 @@ function HomePage() {
 
     return (
 
-        <>
+        <main>
 
-            <h1>well? I think it works</h1>
+            <div>
 
-            <DefinitionsProvider fallback={fallback}>
+                <div>
+                    <h1>Welcome to LFPeek</h1>
+                    <h2>A Destiny 2 Profile and Item Inspector</h2>
+                </div>
 
-                
+                <div>
+                    Get started by looking up a player or an item
+                </div>
 
-            </DefinitionsProvider>
+            </div>
 
-        </>
+        </main>
 
     )
 }
 
-function ExampleItem({itemHash}) {
-    const exampleWep = getInventoryItemLiteDef(itemHash);
-    const icon = exampleWep?.displayProperties.icon; 
+// function ExampleItem({ itemHash }) {
+//     const exampleWep = getInventoryItemLiteDef(itemHash);
+//     const icon = exampleWep?.displayProperties.icon;
 
-    return (
-        <>
-            <img src={`https://www.bungie.net${icon}`}></img>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <img src={`https://www.bungie.net${icon}`}></img>
+//         </>
+//     )
+// }
 
 export default HomePage;
