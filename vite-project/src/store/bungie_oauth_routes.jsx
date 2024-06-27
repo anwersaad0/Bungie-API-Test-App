6ef5b5b649d13b9e6b1eec04b94937b1c1483a97
anwersaad0 <anwer.saad0@gmail.com>
2024-06-27 15:32:21 -0700

    set up route for OAuth

--- vite-project/src/store/bungie_oauth_routes.jsx
+++ vite-project/src/store/bungie_oauth_routes.jsx
@@ -1,2 +1,17 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 const clientId = import.meta.env.VITE_CLIENT_ID;
+
+const GET_AUTH = "/GET_AUTH";
+
+const getAuth = (auth) => ({
+    type: GET_AUTH,
+    auth,
+})
+
+export const getBungieAuth = () => async (dispatch) => {
+    const res = await fetch(`https://www.bungie.net/en/OAuth/Authorize?client_id=${clientId}&response_type=code`);
+
+    if (res.ok) {
+        
+    }
+}
