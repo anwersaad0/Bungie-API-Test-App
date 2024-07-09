43bd2c8aead0230507b2f9cd884bec739344d106
anwersaad0 <anwer.saad0@gmail.com>
2024-07-09 11:24:18 -0700

    added refresh token route

--- vite-project/src/store/bungie_oauth_routes.jsx
+++ vite-project/src/store/bungie_oauth_routes.jsx
@@ -2,26 +2,24 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
-const GET_AUTH = "/GET_AUTH";
+//const GET_AUTH = "/GET_AUTH";
 const GET_TOKEN = "/GET_TOKEN";
+const REFRESH_TOKEN = "/REFRESH_TOKEN";
 
-const getAuth = (auth) => ({
-    type: GET_AUTH,
-    auth,
-})
+// const getAuth = (auth) => ({
+//     type: GET_AUTH,
+//     auth,
+// })
 
 const getToken = (token) => ({
     type: GET_TOKEN,
     token,
 })
 
-// export const getBungieAuth = () => async (dispatch) => {
-//     const res = await fetch(`https://www.bungie.net/en/OAuth/Authorize?client_id=${clientId}&response_type=code`);
-
-//     if (res.ok) {
-        
-//     }
-// }
+const refreshToken = (token) => ({
+    type: REFRESH_TOKEN,
+    token
+})
 
 export const getAuthToken = (code) => async (dispatch) => {
     const res = await fetch("https://www.bungie.net/Platform/App/OAuth/Token/", {
@@ -44,12 +42,36 @@ export const getAuthToken = (code) => async (dispatch) => {
     }
 }
 
+export const refreshAuthToken = (refresh) => async (dispatch) => {
+    const res = await fetch("https://www.bungie.net/Platform/App/OAuth/Token/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            "client_id": clientId,
+            "client_secret": clientSecret,
+            "grant_type": "refresh_token",
+            "refresh_token": refresh,
+        }).toString()
+    })
+
+    if (res.ok) {
+        const data = await res.json();
+        await dispatch(refreshToken(data));
+    }
+}
+
 const initState = {};
 
 function authReducer(state = initState, action) {
     let newState;
     switch(action.type) {
         case GET_TOKEN:
+            newState = {...state};
+            newState[action.token] = action.token;
+            return newState;
+        case REFRESH_TOKEN:
             newState = {...state};
             newState[action.token] = action.token;
             return newState;
