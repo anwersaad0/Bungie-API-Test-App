8ae366a3b769b45d7c795cbca8f2fc3cbd8bc6c5
anwersaad0 <anwer.saad0@gmail.com>
2024-07-10 14:22:56 -0700

    removed account from account page useeffect

--- vite-project/src/components/D2Account/index.jsx
+++ vite-project/src/components/D2Account/index.jsx
@@ -10,7 +10,7 @@ function D2Account() {
 
     useEffect(() => {
         dispatch(getD2CurrentUser(JSON.parse(localStorage.getItem("token")).access_token));
-    }, [account, dispatch]);
+    }, [dispatch]);
 
     console.log('account', account);
 
